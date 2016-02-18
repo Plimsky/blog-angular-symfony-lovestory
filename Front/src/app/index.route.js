27c982c0387550acf8f160fcfc6d4b05af203c47
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2016-02-18 22:04:41 +0100

    Add ui-router header and content elements

--- Front/src/app/index.route.js
+++ Front/src/app/index.route.js
@@ -1,21 +1,30 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
     angular
         .module('blog')
         .config(routerConfig);
 
-  /** @ngInject */
-  function routerConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
-      });
+    /** @ngInject */
+    function routerConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('blog', {
+                url:   '/',
+                views: {
+                    'header':  {
+                        templateUrl:  'app/components/header/header.html',
+                        controller:   'HeaderController',
+                        controllerAs: 'headCtrl'
+                    },
+                    'content': {
+                        templateUrl:  'app/main/main.html',
+                        controller:   'MainController',
+                        controllerAs: 'main'
+                    }
+                }
+            });
 
-    $urlRouterProvider.otherwise('/');
-  }
+        $urlRouterProvider.otherwise('/');
+    }
 
 })();
