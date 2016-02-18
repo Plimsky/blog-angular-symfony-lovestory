632b244e0237788465ded82660c59c1f2e1c4d5e
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2016-02-18 23:26:52 +0100

    Titles are no unique anymore

--- Back/src/AppBundle/Entity/Article.php
+++ Back/src/AppBundle/Entity/Article.php
@@ -24,7 +24,7 @@ class Article
     /**
      * @var string
      *
-     * @ORM\Column(name="title", type="string", length=255, unique=true)
+     * @ORM\Column(name="title", type="string", length=255)
      */
     private $title;
 
